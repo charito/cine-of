27c700ca1f5f09452f10bd8d324043cd8ed7ef45
charito <xarohg@gmail.com>
2016-06-10 17:13:05 -0500

    Categorias de usuario

--- videos.php
+++ videos.php
@@ -11,31 +11,40 @@
        	$cate_resultado=mysqli_query($xc,$sql);
        	$categorias = array();
        	$categorias_vals = array();
-       	$i = 0;
+
        	while ($extraido = mysqli_fetch_array($cate_resultado)) {
-			$categorias[$i] = $extraido['nombre_Categoria'];
+			$categorias[] = $extraido['nombre_Categoria'];
 			$id_categoria = $extraido['id_Categoria'];
 
-			$sql="SELECT p.nombre_Peli, p.duracion_Peli, p.estreno_Peli,p.imagen_Portada_Peli , p.logo_Peli FROM categoria_perfil cp, categoria c, peliculas p, categoria_peliculas ctp WHERE cp.Perfil_id_Perfil = $id_usuario and c.id_Categoria=cp.Categoria_id_Categoria and p.id_Peli=ctp.id_Peli and ctp.id_Categoria=$id_categoria";
-       		$resultadop=mysqli_query($xc,$sql);
+			$sql2="SELECT p.nombre_Peli, p.duracion_Peli, p.estreno_Peli,p.imagen_Portada_Peli ,p.logo_Peli FROM peliculas p, categoria_peliculas ctp WHERE p.id_Peli=ctp.id_Peli and ctp.id_Categoria=$id_categoria";
+       		$resultadop=mysqli_query($xc,$sql2);
 			
-			$j = 0;
+			$peliculas = array();
 			while ($extraidop = mysqli_fetch_array($resultadop)) {
-				$categorias_vals[$i] = array();
-				
-				$categorias_vals[$i][$j] = array();
-				$categorias_vals[$i][$j]["nombre_Peli"] = $extraidop['nombre_Peli'];
-				$categorias_vals[$i][$j]["duracion_Peli"] = $extraidop['duracion_Peli'];
-				$categorias_vals[$i][$j]["estreno_Peli"] = $extraidop['estreno_Peli'];
-				$categorias_vals[$i][$j]["imagen_Portada_Peli"] = $extraidop['imagen_Portada_Peli'];
-				$categorias_vals[$i][$j]["logo_Peli"] = $extraidop['logo_Peli'];
-				$j++;
+				$peli = array();
+				$peli["nombre_Peli"] = $extraidop['nombre_Peli'];
+				$peli["duracion_Peli"] = $extraidop['duracion_Peli'];
+				$peli["estreno_Peli"] = $extraidop['estreno_Peli'];
+
+				/// los blobs se tienen que transformar paara guardarlos en el array
+				$image = imagecreatefromstring($extraidop['imagen_Portada_Peli']); 
+				ob_start(); //start capture of the output buffer
+				imagejpeg($image, null, 80);
+				$data = ob_get_contents();
+				ob_end_clean();
+				$peli["imagen_Portada_Peli"] = $data;
+
+				$image = imagecreatefromstring($extraidop['logo_Peli']); 
+				ob_start(); //start capture of the output buffer
+				imagejpeg($image, null, 80);
+				$data = ob_get_contents();
+				ob_end_clean();
+				$peli["logo_Peli"] = $data;
+				$peliculas[] = $peli;
 			}
-			$i++;
+			$categorias_vals[]=$peliculas;
 
 		}
-
-
     }
        desconectar($xc);
 ?>
@@ -57,48 +66,50 @@
 				<?php
 					require_once("top-header.php");
 				?>
+
 			<div class="right-content">
+
+
+				<?php
+					foreach ($categorias as $key => $value): ?>
+
+					<div class="right-content-heading">
+						<div class="right-content-heading-left">
+							<h3 class="head"><?php echo $value;?></h3>
+						</div>
+					</div>
+
+					<?php
+						$pelis = $categorias_vals[$key];
+						foreach ($pelis as $key_p => $value_p): ?>
+
+						<div class="content-grid">
+							<?php /// este codigo: echo $key."-".$key_p; es para crear un id personal para cada pelicula y poder hacer el popup mostrando la pelicula ?>
+							<a class="play-icon popup-with-zoom-anim" href="#PELIC<?php echo $key."-".$key_p; ?>"">
+								<img src="data:image/jpeg;base64,<?php echo base64_encode($value_p["imagen_Portada_Peli"]); ?>" />
+							</a>
+							<h3><?php echo $value_p["nombre_Peli"]; ?></h3>
+							<ul>
+								<li><a href="#"><img src="images/likes.png" title="likes" /></a></li>
+								<li><a href="#"><img src="images/views.png" title="views" /></a></li>
+								<li><a href="#"><img src="images/link.png" title="link" /></a></li>
+							</ul>
+							<a class="button play-icon popup-with-zoom-anim" href="#PELIC1">Mirar</a>
+							<div id="PELIC<?php echo $key."-".$key_p; ?>" class="mfp-hide">
+								<iframe src="https://www.youtube.com/embed/bXQCptmBQ3c" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+					<?php endforeach;?>
+					<div class="clearfix"> </div>
+
+				<?php endforeach;?>
+				
 				<div class="right-content-heading">
 					<div class="right-content-heading-left">
-						<h3 class="head">Colección de videos</h3>
+						<h3 class="head">M&aacutes videos</h3>
 					</div>
 				</div>
-				<!-- pop-up-box -->
-				<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
-				<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
-				 <script>
-						$(document).ready(function() {
-						$('.popup-with-zoom-anim').magnificPopup({
-							type: 'inline',
-							fixedContentPos: false,
-							fixedBgPos: true,
-							overflowY: 'auto',
-							closeBtnInside: true,
-							preloader: false,
-							midClick: true,
-							removalDelay: 300,
-							mainClass: 'my-mfp-zoom-in'
-						});
-						});
-				</script>
-
-				<!--//pop-up-box -->
-				<?php 
-				echo "cate:";
-					foreach ($categorias as $key => $value) {
-						echo "<h2>$value</h2>";
-						$pelis = $categorias_vals;
-						json_encode($pelis);
-						/*foreach ( as $key_p => $value_p) {
-							echo json_encode($value_p);
-						}*/
-					}
-					echo "end cate";
-				?>
 				
-
-				<div class="content-grids">
-				<h2></h2>
 					<div class="content-grid">
 
 						<a class="play-icon popup-with-zoom-anim" href="#PELI1"><img src="images/poster/1.jpg" title="allbum-name" /></a>
@@ -110,56 +121,6 @@
 						</ul>
 						<a class="button play-icon popup-with-zoom-anim" href="#PELI1">Mirar</a>
 					</div>
-
-					<div id="PELI1" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/bXQCptmBQ3c" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI2" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/FXK0BCG807Y" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI3" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/JYO3N7LpQ6g" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI4" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/ZLbY5nxzGbY" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI5" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/2vW02jfBhjs" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-
-					<div id="PELI6" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/eJk1OtFvl1s" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI7" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/jU2NBOTF7Qg" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI8" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/jbJ7jM-m0ic" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI9" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/NGwAu4g_1PY" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI10" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/zEGcjQ2Cr3M" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI11" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/3NmUk-y9Ri8" frameborder="0" allowfullscreen></iframe>
-					</div>
-
-					<div id="PELI12" class="mfp-hide">
-						<iframe  src="https://www.youtube.com/embed/469a5aWNhKg" frameborder="0" allowfullscreen></iframe>
-					</div>
-
 
 					<div class="content-grid">
 						<a class="play-icon popup-with-zoom-anim" href="#PELI2"><img src="images/poster/2.jpg" title="allbum-name" /></a>
@@ -234,7 +195,7 @@
 					
 				
 					<div class="clearfix"> </div>
-					<start-pagenation>
+					
 					<div class="pagenation">
 						<ul>
 							<li><a href="#">1</a></li>
@@ -248,6 +209,75 @@
 					<div class="clearfix"> </div>
 					
 				</div>
+
+
+				<!-- pop-up-box -->
+				<link href="css/popuo-box.css" rel="stylesheet" type="text/css" media="all" />
+				<script src="js/jquery.magnific-popup.js" type="text/javascript"></script>
+				 <script>
+						$(document).ready(function() {
+						$('.popup-with-zoom-anim').magnificPopup({
+							type: 'inline',
+							fixedContentPos: false,
+							fixedBgPos: true,
+							overflowY: 'auto',
+							closeBtnInside: true,
+							preloader: false,
+							midClick: true,
+							removalDelay: 300,
+							mainClass: 'my-mfp-zoom-in'
+						});
+						});
+				</script>
+
+				<div id="PELI1" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/bXQCptmBQ3c" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI2" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/FXK0BCG807Y" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI3" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/JYO3N7LpQ6g" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI4" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/ZLbY5nxzGbY" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI5" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/2vW02jfBhjs" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+
+					<div id="PELI6" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/eJk1OtFvl1s" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI7" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/jU2NBOTF7Qg" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI8" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/jbJ7jM-m0ic" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI9" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/NGwAu4g_1PY" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI10" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/zEGcjQ2Cr3M" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI11" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/3NmUk-y9Ri8" frameborder="0" allowfullscreen></iframe>
+					</div>
+
+					<div id="PELI12" class="mfp-hide">
+						<iframe  src="https://www.youtube.com/embed/469a5aWNhKg" frameborder="0" allowfullscreen></iframe>
+					</div>
 					
 	<?php
 		require_once("footer.php");
