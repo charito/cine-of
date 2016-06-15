7cefe91b3f889f570a7085a4a8bdf681618c36d6
BrianPareja <benjytj2@gmail.com>
2016-06-15 00:11:48 -0500

    agregando pop de las noticias

--- index.php
+++ index.php
@@ -196,9 +196,7 @@ $k=$k+1;
 	<div class="clear"></div>
 
 	<!-- MODAL DE NOTICIAS -->
-	<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-  Launch demo modal
-</button>
+	
 
 <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
