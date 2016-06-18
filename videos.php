adaedbaf07d8e04840ccaaa1d1c1bef9977d8624
charito <xarohg@gmail.com>
2016-06-18 18:43:23 -0500

    commit

--- videos.php
+++ videos.php
@@ -169,7 +169,7 @@
 								<li><a href="#"><img src="images/views.png" title="views" /></a></li>
 								<li><a href="#"><img src="images/link.png" title="link" /></a></li>
 							</ul>
-							<a class="button play-icon popup-with-zoom-anim" href="#PELIC<?php echo $key."-peli"; ?>">Mirar</a>
+							<a class="button" href="single.php">Ver más</a>
 							<div id="PELIC<?php echo $key."-peli"; ?>" class="mfp-hide">
 								<iframe src="<?php echo $value["url_trailer"]; ?>" frameborder="0" allowfullscreen></iframe>
 							</div>
